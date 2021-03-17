081be4e1e5d6cf078e13916c5b773a52453dc695
HarrisLim <harris19921204@gmail.com>
2021-03-18 07:47:53 +0900

    출력하기08

---
+++ CodeUp/1008.c
@@ -0,0 +1,9 @@
+/* \u는 유니코드 이용 */
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    printf("\u250C\u252C\u2510\n");
+    printf("\u251C\u253C\u2524\n");
+    printf("\u2514\u2534\u2518\n");
+    return 0;
+}
