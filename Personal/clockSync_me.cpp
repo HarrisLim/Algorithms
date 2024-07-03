94aafa88ca235ef7462608a939bfcb3b8412a5e6
HarrisLim <harris19921204@gmail.com>
2024-07-03 21:58:01 +0900

    recursive function

--- Personal/clockSync_me.cpp
+++ Personal/clockSync_me.cpp
@@ -16,14 +16,14 @@ const char linked[SWITCHES][CLOCKS+1] = {
     ".xxxxx..........",
     "...xxx...x...x.."};
 
-bool areAligned(const vector<int>& clocks)
+bool areAligned(vector<int>& clocks)
 {
-    bool ret = false;
+    bool ret=false;
     for(int i=0; i<clocks.size(); i++)
     {
         if(clocks.at(i) == 12) ret = true;
         else ret = false;
-    }
+    }   
     return ret;
 }
 
@@ -33,8 +33,8 @@ void push(vector<int>& clocks, int swtch)
     {
         if(linked[swtch][clock] == 'x')
         {
-            clocks[clock] +=3;
-            if(clocks[clock] == 15) clocks[clock] =3;
+            clocks[clock] += 3;
+            if(clocks[clock] == 15) clocks[clock] = 3;
         }
     }
 }
@@ -42,15 +42,15 @@ void push(vector<int>& clocks, int swtch)
 int solve(vector<int>& clocks, int swtch)
 {
     if(swtch == SWITCHES) return areAligned(clocks) ? 0 : INF;
+
     int ret = INF;
-    for(int cnt =0; cnt<4; cnt++)
+    for(int cnt=0; cnt<4; cnt++)
     {
         ret = min(ret, cnt+solve(clocks, swtch+1));
         push(clocks, swtch);
     }
     return ret;
 }
-
 int main()
 {
     // vector<int> clocks = {12, 6, 6, 6, 6, 6, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12};
