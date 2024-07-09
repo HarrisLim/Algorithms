60e885b5bac4248f5c9a66cb062b6c65eb8c6365
HarrisLim <harris19921204@gmail.com>
2024-07-09 21:07:36 +0900

    add memoization

--- JongmanBook/bino.cpp
+++ JongmanBook/bino.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
-/*
-this is source about binomial coeffcient
-*/
+
+int cache[30][30];
+
+// this is source about binomial coeffcient with dup exploring
 int bino(int n, int r)
 {
     // base case: n=r(the case pick every elements) or r=0(the case there isnt any element)
@@ -11,8 +13,21 @@ int bino(int n, int r)
     return bino(n-1, r-1) + bino(n-1, r);
 }
 
+// solve with memoization
+int binoMemo(int n, int r)
+{
+    if(r==0 || n==r) return 1;
+    if(cache[n][r] != 0)
+    {
+        return cache[n][r];
+    }
+    
+    return cache[n][r] = binoMemo(n-1, r-1) + binoMemo(n-1, r);
+}
+
 int main()
 {
-    cout << "ret: " << bino(3,2) << endl;
+    cout << "ret_1: " << bino(4,2) << endl;
+    cout << "ret_2: " << binoMemo(4,2) << endl;
     return 0;
 }
