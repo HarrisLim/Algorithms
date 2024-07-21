c24c5ef6634a19f8fbc6fb105d44b3caa4b775d0
HarrisLim <harris19921204@gmail.com>
2024-07-21 13:22:15 +0900

    [Unsolved] 100335. Vowels Game in a String

---
+++ LeetCode/weeklyContest_407-2.cpp
@@ -0,0 +1,10 @@
+/* 100335. Vowels Game in a String */
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        // base case
+        if(s.size() == 0) return 0;
+
+        if()
+    }
+};
