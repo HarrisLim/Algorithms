98fd419b9eca0eb231489dadfaad4bee55b94a2d
HarrisLim <harris19921204@gmail.com>
2024-07-21 14:37:17 +0900

    [Unsolved] 100335. Vowels Game in a String

--- LeetCode/weeklyContest_407-2.cpp
+++ LeetCode/weeklyContest_407-2.cpp
@@ -8,3 +8,18 @@ public:
         if()
     }
 };
+
+/* this is the code from almost leetcoder */
+bool doesAliceWin(string s) {
+    // store vowel
+    string v = "aeiou";
+    // if the string has at least one vowel, Alice win.
+    // because, always Alice has a first turn and pick odd numbers
+    // then, Bob has to pick even number of vowel. but Alice left just one vowel
+    // so Bob has no vowel to pick :(
+    for(char c: s)
+        for(char t : v)
+            if(t==c) return true;
+    
+    return false;
+}
