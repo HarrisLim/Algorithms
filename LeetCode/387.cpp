acb84480feb457d12fc1aa0e893ecae98b264561
HarrisLim <harris19921204@gmail.com>
2024-09-06 22:29:48 +0900

    387. First Unique Character in a String

--- LeetCode/387.cpp
+++ LeetCode/387.cpp
@@ -2,18 +2,14 @@ class Solution {
 public:
     int firstUniqChar(string s) {
         int n = s.size(), ret = -1;
-        map<char, int> mp;
-        
+        unordered_map<char, int> mp;
+
         for(int i=0 ;i<n; i++)
             mp[s[i]]++;
 
-        while(n>0)
-        {
-            if(mp[s[n-1]] == 1)
-                ret=n-1;
-            
-            n--;
-        }
+        for(int i=0; i<n; i++)
+            if(mp[s[i]] == 1)
+                return i;
 
         return ret;
     }
