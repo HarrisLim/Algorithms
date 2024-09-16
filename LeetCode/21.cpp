a2158c6f35d3dc9adda0e29fa64f0734f4a94b0b
HarrisLim <harris19921204@gmail.com>
2024-09-16 22:18:11 +0900

    21. Merge Two Sorted Lists

--- LeetCode/21.cpp
+++ LeetCode/21.cpp
@@ -1,3 +1,4 @@
+// ## The First Approach
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
@@ -39,5 +40,55 @@ public:
         }
 
         return mergedList;
+    }
+};
+
+// ## The Second Approach
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* dummy = new ListNode();
+        ListNode* merged = dummy;
+
+        while(list1 && list2)
+        {
+            if(list1->val > list2->val)
+            {
+                merged->next = list2;
+                list2 = list2->next;
+            }
+            else
+            {
+                merged->next = list1;
+                list1 = list1->next;
+            }
+            merged = merged->next;
+        }
+
+        merged->next = list1 ? list1 : list2;
+
+        return dummy->next;
+    }
+};
+
+// ## The Third Approach
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+
+        // base cases
+        if(!list1 || !list2)
+            return list1 ? list1 : list2;
+        
+        if(list1->val > list2->val)
+        {
+            list2->next = mergeTwoLists(list1, list2->next);
+            return list2;
+        }
+        else
+        {
+            list1->next = mergeTwoLists(list1->next, list2);
+            return list1;
+        }
     }
 };
