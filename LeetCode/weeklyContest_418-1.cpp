d3a7f7fe4fd0304aacea1fa5f1fc92a3c39dc1eb
HarrisLim <harris19921204@gmail.com>
2024-10-06 17:20:11 +0900

    Q1. Maximum Possible Number by Binary Concatenation

---
+++ LeetCode/weeklyContest_418-1.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string toBinary(int n)
+    {
+        string str="";
+        while(n>0)
+        {
+            str += to_string(n%2);
+            n/=2;
+        }
+        reverse(str.begin(), str.end());
+        return str;
+    }
+    int maxGoodNumber(vector<int>& nums) {
+        vector<string> strNums(3);
+        for(int i=0; i<3; i++)
+            strNums[i] = toBinary(nums[i]);
+
+        string tempRet="";
+        for(int i=0; i<3; i++)
+        {
+            for(int j=0; j<3; j++)
+            {
+                for(int k=0; k<3; k++)
+                {
+                    if(i!=j && j!=k && k!=i)
+                    {
+                        string combine = strNums[i] + strNums[j] + strNums[k];
+                        tempRet = max(tempRet, combine);
+                    }
+                }
+            }
+        }
+        int ret = stoi(tempRet, nullptr, 2);
+        return ret;
+    }
+};
